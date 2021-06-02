820c4d46736fb261c8305e295acb2a2a96c9d4fa
ATSqueak <67633294+ATSqueak@users.noreply.github.com>
2021-06-02 11:23:29 +0100

    Update Palindrome.py
    Added a check for whether entered value is a string or not

--- Palindrome.py
+++ Palindrome.py
@@ -19,9 +19,11 @@ print('You entered the words: ', list_text)
 for i in range(length_of_array):
     individual_word = list_text[i]
     reverse_individual_word = individual_word[::-1]
-    if individual_word == reverse_individual_word:
-        print('The word "{}" is a palindrome'.format(individual_word))
+    if individual_word.isalpha():
+        if individual_word == reverse_individual_word:
+            print('The word "{}" is a palindrome'.format(individual_word))
+        else:
+            print('The word "{}" is not a palindrome'.format(individual_word))
     else:
-        print('The word "{}" is not a palindrome'.format(individual_word))
-
+        print('The entered value "{}" is not a word'.format(individual_word))
 
